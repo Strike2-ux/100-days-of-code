d63621ddeea44aa7cf169dc5b5e832a67781c656
Paul P Levy <69823554+Strike2-ux@users.noreply.github.com>
2023-01-27 17:38:36 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+print("Hello Replit")
+print("and how are you?")
+print("😆")
+
+print("""
+Paul
+January 27th 2023
+I am signing up for Replit's 100 days of Python
+challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do
+this from my phone wherever I happen to be. No excuses
+for not coding from the middle of a field!
+I am feeling 🤠
+
+You can follow my progress at https://replit.com/@Strike2-ux/day-1100-days
+
+
+
+""")
+
+print("Please, Work")
