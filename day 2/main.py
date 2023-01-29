9a4b494e83c6d2b24e02ebfbefaa8cc69267914c
Strike2-ux <13paullevy@gmail.com>
2023-01-29 19:03:59 -0300

    day 2 of the challenge

---
+++ day 2/main.py
@@ -0,0 +1,9 @@
+print("Getting to know you!")
+myName = input("Whats your name?: ")
+myFood = input("Whats your favorite food?: ")
+myMusic = input("Whats your favorite music?: ")
+myLocation = input("Where do you live?: ")
+
+print("You are " + myName + " you probably are hungry for " + myFood +
+      " and you're definitly getting your groove from " + myMusic +
+      " living in " + myLocation)
