29a1950e2e5723af86612a3f2e34a01ba80dc837
Strike2-ux <13paullevy@gmail.com>
2023-01-30 00:54:29 -0300

    day 3 of the challenge

---
+++ day 3/main.py
@@ -0,0 +1,9 @@
+yourName= input("Name: ")
+whatYear = input("what year is it?: ")
+favPokemon = input("what pokemon do you want to start this adventure?: ")
+print(yourName, "thinks it is year ", whatYear, favPokemon, """  will be
+
+the one who 
+            will change your 
+                              
+                                    mind """)
