a6bd442f41525824426a5af1259a71447f8a038d
Strike2-ux <13paullevy@gmail.com>
2023-02-05 11:25:47 -0300

    uptade01

---
+++ day 5/main.py
@@ -0,0 +1,25 @@
+print()
+print("""Marvel Movie Character Creator
+--""")
+
+character = input("Do you like 'hanging around'?: ")
+if character == "no":
+    print("then you're not spiderman")
+if character == "yes":
+    print("Hi spiderman")
+else:
+    print("Who cares")
+
+
+myName = input("Whats your name?: ")
+if myName == "David":
+    print("Welcome dude!")
+    print("You're just the baldest dude I've ever seen")
+else:
+    print("Who on earth are you?!")
+
+catsOrDogs = input("Are you a cat person? Or a dog person?: ")
+if catsOrDogs == "cats":
+    print("Meow")
+else:
+    print("Woof")
