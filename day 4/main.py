22a80474b7b95e0d6a7a4f559331e6ec63a99f0c
Strike2-ux <13paullevy@gmail.com>
2023-02-04 23:30:51 -0300

    uptade

---
+++ day 4/main.py
@@ -0,0 +1,12 @@
+print("=== Your Adventure Simulator")
+print("""" You'll be asked a bunch of questions 
+then we'll make you up an amazing 
+story with YOU as the star! 🤯""")
+print()
+name = input("You're name: ")
+enemy= input("You're worst enemy name: ")
+superPower= input("Your super power: ")
+print()
+print("Our story begins as our hero name approaches a forbiden castle...  ")
+print("Sudedenly a bolt of lightning striked ground in the feet of", name)
+print("\033[31m'Our final battle begins!'\033[0m shouts the evil", enemy, "clearly missing the fact that", name, "has the power of \033[35m", superPower, "\033[0mwhich means they'll win quite easily")
