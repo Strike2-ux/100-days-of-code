a73ee36e2b46c00cd462f84c1bc1523d26454316
Strike2-ux <13paullevy@gmail.com>
2023-02-10 01:58:54 -0300

    updating 3.0

---
+++ day 6/main.py
@@ -0,0 +1,11 @@
+print(""" MY LOGIN SYSTEM
+        ++++++++++++++++
+          """)
+Username = input("Enter you login ")
+Password = input("insert your password ")
+if Username == "Paul" and Password == "yolo":
+    print("Welcome paul")
+elif Username == "secret":
+    print("you found the ultimate secret")
+else:
+    print("You Fail!")
---
+++ day 7/main.py
@@ -0,0 +1,12 @@
+tvShow = input("What is your favourite tv show? ")
+if tvShow == "peppa pig":
+    print("Ugh, why?")
+    faveCharacter = input ("Who is your favourite character? ")
+    if faveCharacter == "mommy pig" :
+        print("right answer")
+    else:
+        print("Nah, Mommy Pig's the greatest")
+elif tvShow == "paw patrol":
+    print("Aww, sad times")
+else:
+    print("Yeah, that's cool and all....")
